ef1e3bd4aecb6d81d3c703e12b660691b191eefd
Egor_Ostroumov <araimo@yandex.ru>
2022-09-17 20:48:43 +0300

    30:54 hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  let [likes, setLikes] = useState(0)
-  let [value, setValue] = useState('текст в инпуте')
+    let [value, setValue] = useState('текст в инпуте')
 
-  function increment() {
-    setLikes(likes += 1)
-  }
-
-  function decrement() {
-    setLikes(likes -= 1)
-  }
-
-  return (
-    <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <h1>{value}</h1>
-      <input type='text'
-             value={value}
-             onChange={event => setValue(event.target.value)}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Counter/>
+            <h1>{value}</h1>
+            <input type='text'
+                   value={value}
+                   onChange={event => setValue(event.target.value)}/>
+        </div>
+    );
 }
 
 export default App;

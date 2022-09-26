1192bb9548b7a0e2ed5cea64dcbbeb37b4b17783
Egor_Ostroumov <araimo@yandex.ru>
2022-09-26 21:27:29 +0300

    сортировка, выпадающий список

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, {useRef, useState} from "react";
-import Counter from "./components/Counter";
+import React, {useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/myInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
+    const [selectedSort, setSelectedSort] = useState('')
     const [posts, setPosts] = useState([])
 
     const createPost = (newPost) => {
@@ -18,10 +17,27 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
+
     return (
         <div className="App">
             {/*<Counter/>*/}
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'},
+                    ]}
+                />
+            </div>
             {posts.length !== 0
                 ? <PostList remove={removePost} posts={posts} title="Список постов 1"/>
                 : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>

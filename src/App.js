ad93b63caee950f2205eed450b426b6ae81cd2e9
Egor Ostroumov <araimo@yandex.ru>
2022-09-30 17:44:18 +0300

    модальное окно 1:23:50

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
 
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
 
     const sortedPosts = useMemo(() => {
         console.log('!getSortedPosts!')
@@ -23,6 +26,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     // получаем post из дочернего компонента
@@ -33,7 +37,12 @@ function App() {
     return (
         <div className="App">
             {/*<Counter/>*/}
-            <PostForm create={createPost}/>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Создать пост
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
             <PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов 1"/>
